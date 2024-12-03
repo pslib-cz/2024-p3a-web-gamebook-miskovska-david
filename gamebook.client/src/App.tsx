3a155390fee90104533851702485679211efac5f
MiskovskaM <marija.miskovska.022@pslib.cz>
2024-12-03 21:56:49 +0100

    login-props

--- gamebook.client/src/App.tsx
+++ gamebook.client/src/App.tsx
@@ -6,7 +6,7 @@ import Login from "./pages/Login/Login";
 
 function App() {
     return (
-       <Login></Login>
+       <Login title='Přihlásit se' placeholder='Uživatelské jméno / Email' placeholder2='Heslo' btnText='Přihlásit se'></Login>
     )
 
 }
--- gamebook.client/src/pages/Login/Login.tsx
+++ gamebook.client/src/pages/Login/Login.tsx
@@ -1,17 +1,24 @@
 import style from "./Login.module.css";
 
-const Home = () =>{
+type LoginProps = {
+    title: string;
+    placeholder: string;
+    btnText: string;
+    placeholder2: string;
+}
+
+const Login: React.FC<LoginProps> = ({title, placeholder, btnText, placeholder2}) =>{
     return (
         <div className={style.login__screen}>
             <div className={style.login__container}>
-                <h2>Přihlásit se</h2>
+                <h2>{title}</h2>
                 <form className={style.login__form}>
-                    <input type="text" placeholder="Uživatelské jméno / Email" className={style.login__input}></input>
-                    <input type="text" placeholder="Heslo" className={style.login__input}></input>
+                    <input type="text" placeholder={placeholder} className={style.login__input}></input>
+                    <input type="text" placeholder={placeholder2} className={style.login__input}></input>
                 </form>
-                <button className={style.login__btn}>Přihlásit se</button>
+                <button className={style.login__btn}>{btnText}</button>
             </div>
         </div>
     )
 }
-export default Home;
+export default Login;
