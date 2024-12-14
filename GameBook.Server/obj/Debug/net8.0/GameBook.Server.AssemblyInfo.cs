c959929840f46d9d1760356ac9d3b47d41d3b9c9
MiskovskaM <marija.miskovska.022@pslib.cz>
2024-12-14 01:59:52 +0100

    spinkaat

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82bb425c8cbba7e1d084cd064919dee6b93fcb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8c0d780f998b632b5503a1029db48c8a54c665")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
