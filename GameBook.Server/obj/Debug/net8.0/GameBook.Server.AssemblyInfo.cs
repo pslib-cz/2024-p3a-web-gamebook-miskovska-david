d82bb425c8cbba7e1d084cd064919dee6b93fcb9
MiskovskaM <marija.miskovska.022@pslib.cz>
2024-12-14 01:54:32 +0100

    dobrou<3

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1628020f9fb64e509bff1f16f8fa1252e38cdc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65104170298c885b818acb00d9ffd9c6cb596619")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
