4b21338144462897b3f2dc6700139b11267c5554
Tomas-David <ttomikdavid@gmail.com>
2024-12-14 09:34:52 +0100

    oprava obrázků

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8c0d780f998b632b5503a1029db48c8a54c665")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c959929840f46d9d1760356ac9d3b47d41d3b9c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
