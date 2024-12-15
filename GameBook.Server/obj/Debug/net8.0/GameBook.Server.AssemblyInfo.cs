7d67fd2c6fb69abc6bb878b9ae494cd12c4db6e2
MiskovskaM <marija.miskovska.022@pslib.cz>
2024-12-15 23:19:52 +0100

    room01 a room02

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed874dd7a50767c0d3041de7aeefbc8f4a836c13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c53373482cb663250f22d30970b90ddd884db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- gamebook.client/src/pages/Room1/Room02.tsx
+++ gamebook.client/src/pages/Room1/Room02.tsx
@@ -1,68 +1,64 @@
-import style from "./Room01.module.css";
+import style from "./Room02.module.css";
 import React, { useEffect, useState } from "react";
 import Pokracovat from "../../components/Button/Pokracovat";
 import { RoomType } from "../../types";
 
-const Room01: React.FC = () => {
-    const [rooms, setRooms] = useState<RoomType | null>(null);
-    const [error, setError] = useState<Error | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [isTextComplete, setIsTextComplete] = useState<boolean>(false);
-
+const Room02: React.FC= () => {
+    const [rooms, setRooms] = useState<RoomType | null>(null)
+    const [error, setError] = useState<Error | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+    const [visibleDialogIndex, setVisibleDialogIndex] = useState(0);
+    
     useEffect(() => {
         const fetchData = async () => {
-            setLoading(true);
-            try {
-                const response = await fetch("/api/Room/rooms/1"); // ID místnosti je 1
-                if (!response.ok) {
-                    throw new Error("Nepodařilo se načíst místnosti");
-                }
-                const data = await response.json();
-                setRooms(data);
-            } catch (error) {
-                if (error instanceof Error) {
-                    setError(error);
-                } else {
-                    setError(new Error("Něco se pokazilo"));
-                }
-            } finally {
-                setLoading(false);
+            setLoading(true)
+            try{
+                const response = await fetch("/api/Room/rooms/2"); //Ta 1 je id místnosti, kterou chceme načíst. To pak se bude měnit v routě, takže místo 1 tam bude proměnná.
+            if(!response.ok){
+                throw new Error("Nepodařilo se načíst místnosti")
             }
-        };
+            const data = await response.json();
+            setRooms(data)
+            }
+            catch(error){
+                if(error instanceof Error){
+                    setError(error)
+                }else{
+                    setError(new Error("Něco se pokazilo"))
+                }
+            }finally{
+                setLoading(false)
+            }
+        }
         fetchData();
     }, []);
+   console.log(error);
+   console.log(loading);
+   useEffect(() => {
+    if (rooms?.dialogs && visibleDialogIndex < 2) {
+      const currentText = rooms.dialogs[visibleDialogIndex]; // Získání aktuální věty
+      const animationDuration = currentText.length * 70; 
+      const timer = setTimeout(() => {
+        setVisibleDialogIndex((prevIndex) => prevIndex + 1);
+      }, animationDuration);
+      return () => clearTimeout(timer);
+    }
+  }, [visibleDialogIndex, rooms]);
 
-    console.log(error);
-    console.log(loading);
+  return(
+    <div
+    className={style.room__screen}
+    style={{ backgroundImage: `url(/${rooms?.background}` }}>
+    <div className={style.room__container}>
+      {rooms?.dialogs.slice(0, visibleDialogIndex + 1 ).map((dialog, index) => (
+        <p key={index} className={style.room__text}>
+          {dialog}
+        </p>
+      ))}
+      {visibleDialogIndex >= 2 && <Pokracovat text="Pokračovat" />}
+    </div>
+  </div>
+  );
 
-    useEffect(() => {
-        if (rooms?.dialogs && !isTextComplete) {
-            const currentText = rooms.dialogs[0];
-            const animationDuration = currentText.length * 70;
-
-            const timer = setTimeout(() => {
-                setIsTextComplete(true);
-            }, animationDuration);
-
-            return () => clearTimeout(timer);
-        }
-    }, [isTextComplete, rooms]);
-
-    return (
-        <div
-            className={style.room__screen}
-            style={{ backgroundImage: `url(/${rooms?.background})` }}
-        >
-            <div className={style.room__container}>
-                {rooms && (
-                    <p className={style.room__text}>
-                        {rooms.dialogs[0]}
-                    </p>
-                )}
-                {isTextComplete && <Pokracovat text="Pokračovat" />}
-            </div>
-        </div>
-    );
-};
-
-export default Room01;
+}
+export default Room02;
