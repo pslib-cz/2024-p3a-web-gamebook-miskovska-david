cb1a9a60303f4a305db1ac1c6bade33a2b6fadce
Tomas-David <ttomikdavid@gmail.com>
2024-12-16 15:12:28 +0100

    router

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b4a4555a9b4525a56db7bb981c4599f6e8e922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d95d416dd8b4ffaf39deb9b173c29074d7b351")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
