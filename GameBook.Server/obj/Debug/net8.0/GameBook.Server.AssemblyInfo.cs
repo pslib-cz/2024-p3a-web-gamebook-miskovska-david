be92dfafb6e99592f3d880f7c4042c7caf9a74b3
MiskovskaM <marija.miskovska.022@pslib.cz>
2025-01-09 16:21:55 +0100

    uprava db, format webp

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039c64d866304da2ed3727b4c833f51d1070df25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786bac7aa6216bcbbc73ecf13f35b48e9b8f6939")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
