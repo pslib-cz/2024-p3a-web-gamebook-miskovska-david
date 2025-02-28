4ff52140dde92688b955e6bed79c50dc950e4115
Tomas-David <ttomikdavid@gmail.com>
2025-02-28 17:30:37 +0100

    fight logika

--- gamebook.client/src/pages/room/FightRoom.tsx
+++ gamebook.client/src/pages/room/FightRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import {useNavigate, useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import classes from "./FightRoom.module.css";
 import FightButton from "../../components/fightButtons/FightButton";
@@ -8,9 +8,10 @@ import { useEffect, useState } from "react";
 const FightRoom: React.FC = () => {
   const id = useParams().id;
   const idNumber = id ? parseInt(id) : 0;
-
+  const navigate = useNavigate();
   const [fightBtn, setFigntBtn] = useState<JSX.Element | null>(null);
-
+  const [enemyStartHp, setStartEnemyHp] = useState<number>(100);
+  const [playerStartHp, setStartPlayerHp] = useState<number>(100);
   const { data: RoomData } = useFetch<RoomType>(`/api/Room/rooms/${idNumber}`);
   const { data: AtkData } = useFetch<ItemType>(`/api/Item/items/100`);
   const { data: DefData } = useFetch<ItemType>(`/api/Item/items/101`);
@@ -21,24 +22,35 @@ const FightRoom: React.FC = () => {
     `/api/Player/players/${localStorage.getItem("UserId")}`
   );
 
+  
+
   const Attack = (
     playerAtk: number,
-    enemyHp: number
   ) => {
-      console.log(enemyHp -= playerAtk)
     RandomSpawn();
+    setStartEnemyHp((prevHp) => prevHp - playerAtk)
   };
 
   const Defend = (
     defence: number,
-    hp: number,
     enemyAtk: number
   ) => {
-     console.log(hp -= enemyAtk - defence);
+     setStartPlayerHp((prevHp) => prevHp - (enemyAtk - defence));
     RandomSpawn();
   };
 
   const RandomSpawn = () => {
+
+    if(playerStartHp <= 0){
+      navigate("room-with-text/8");
+      return;
+    }
+
+    if(enemyStartHp <= 0){
+      navigate("room-with-text/7");
+      return;
+    }
+
     const screenWith = window.innerWidth;
     const screenHeigh = window.innerHeight;
     const randomTop = Math.floor(Math.random() * (screenHeigh - 150));
@@ -51,7 +63,7 @@ const FightRoom: React.FC = () => {
           top={randomTop}
           left={randomLeft}
           path={`/${DefData?.background}`}
-          OnClick={() => Defend(player?.defense, player?.hp, enemy?.attack)}
+          OnClick={() => Defend(player?.defense ? player.defense : 0, enemy?.attack ? enemy.attack : 10)}
         />
       );
     } else {
@@ -60,18 +72,22 @@ const FightRoom: React.FC = () => {
           top={randomTop}
           left={randomLeft}
           path={`/${AtkData?.background}`}
-          OnClick={() => Attack(player?.attack, enemy?.hp)}
+          OnClick={() => Attack(player?.damage ? player.damage : 10)}
         />
       );
     }
   };
 
+
+
   useEffect(() => {
     RandomSpawn();
+    setStartEnemyHp(enemy?.hp ? enemy.hp : 100);
+    setStartPlayerHp(player?.hp ? player.hp : 100);
   }, []);
 
-  console.log(RoomData?.background);
-  
+  console.log(enemyStartHp);
+  console.log(playerStartHp);
 
   return (
     <div
